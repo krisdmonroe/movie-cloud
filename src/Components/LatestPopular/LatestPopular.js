1aadba3f9e3f7966b303037905826652c502457b
Kris Monroe <50418122+krisdmonroe@users.noreply.github.com>
2020-08-21 18:09:38 -0700

    First Commit

---
+++ src/Components/LatestPopular/LatestPopular.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Axios from 'axios';
+
+function LatestPopular() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Axios from 'axios';
+
+function LatestPopularList() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
