e00f725003cfdc9e0030371303056ea9b87068b4
Kris Monroe <50418122+krisdmonroe@users.noreply.github.com>
2020-08-24 14:24:05 -0700

    Added dynamic images for movies

--- src/Components/LatestPopular/LatestPopular.js
+++ src/Components/LatestPopular/LatestPopular.js
@@ -1,11 +1,13 @@
 import React, {useEffect} from 'react';
 import { fetchPopular } from '../actions/action';
+import axios from 'axios'
 
 function LatestPopular(props){
   console.log('this is props', props.data)
   return (
     <div>
       <h1>{props.data.title}</h1>
+      <img src={`https://image.tmdb.org/t/p/original/${props.data.backdrop_path}` } />
     </div>
   );
 }
--- src/Components/actions/action.js
+++ src/Components/actions/action.js
@@ -10,6 +10,7 @@ export const fetchPopular = () => dispatch => {
     axios
     .get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`)
     .then(res => {
+        console.log('this is res', res)
         dispatch({ type:FETCHING_POPULAR_SUCCESS, payload: res.data.results });
     })
     .catch(err => {
